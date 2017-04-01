11e4ceb63c6c40c85e6c4a2ce004cb8bf6a5083b
Akond Rahman <akond.rahman.buet@gmail.com>
2017-04-01 15:16:16 -0400

    COMM

--- git_process_extractor.py
+++ git_process_extractor.py
@@ -4,3 +4,24 @@ Process extractor from git repositories
 April 01, 2017
 '''
 import os, subprocess, numpy as np
+
+
+
+def getCommitCount(param_file_path, repo_path):
+   totalCountForChurn = 0
+
+   cdCommand            = "cd " + repo_path + " ; "
+   theFile              = os.path.relpath(param_file_path, repo_path)
+   commitCommand        = "git log  --format=%cd " + theFile + " | awk '{ print $2 $3 $5}' | sed -e 's/ /,/g'"
+   command2Run          = cdCommand + commitCommand
+
+   dt_churn_output = subprocess.check_output(['bash','-c', command2Run])
+   dt_churn_output = dt_churn_output.split('\n')
+   dt_churn_output = [x_ for x_ in dt_churn_output if x_!='']
+   totalCountForChurn = len(dt_churn_output)
+   print totalCountForChurn
+   return totalCountForChurn
+
+def getProcessMetrics(file_path_p, repo_path_p):
+    #get commit count
+    COMM = getCommitCount(file_path_p, repo_path_p)
--- main_extarctor.py
+++ main_extarctor.py
@@ -34,5 +34,8 @@ testing purpose
 test_hg_file  = '/Users/akond/PUPP_REPOS/mozilla-releng-downloads/relabs-puppet/manifests/site.pp'
 test_git_file = '/Users/akond/PUPP_REPOS/wikimedia-downloads/mariadb/manifests/heartbeat.pp'
 git_repo_path = '/Users/akond/PUPP_REPOS/wikimedia-downloads/mariadb'
-hg_repo_path = '/Users/akond/PUPP_REPOS/mozilla-releng-downloads/relabs-puppet/'
-getAllProcessMetricsForSingleFile(test_hg_file, hg_repo_path)
+hg_repo_path  = '/Users/akond/PUPP_REPOS/mozilla-releng-downloads/relabs-puppet/'
+
+
+# getAllProcessMetricsForSingleFile(test_hg_file, hg_repo_path)
+getAllProcessMetricsForSingleFile(test_git_file, git_repo_path)
