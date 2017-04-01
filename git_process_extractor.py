de06c0fcd665ea766cf8408fa2b127dbffbbc3ba
Akond Rahman <akond.rahman.buet@gmail.com>
2017-04-01 15:58:30 -0400

    age

--- git_process_extractor.py
+++ git_process_extractor.py
@@ -19,9 +19,47 @@ def getCommitCount(param_file_path, repo_path):
    dt_churn_output = dt_churn_output.split('\n')
    dt_churn_output = [x_ for x_ in dt_churn_output if x_!='']
    totalCountForChurn = len(dt_churn_output)
-   print totalCountForChurn
+   #print totalCountForChurn
    return totalCountForChurn
+
+def calculateMonthDiffFromTwoDates(early, latest):
+    from datetime import datetime
+    early_year   = early.split('-')[0]
+    latest_year  = latest.split('-')[0]
+    early_month  = early.split('-')[-1]
+    latest_month = latest.split('-')[-1]
+    early_dt     = datetime(int(early_year), int(early_month), 1)
+    latest_dt    = datetime(int(latest_year), int(latest_month), 1)
+
+    return (latest_dt.year - early_dt.year)*12 + latest_dt.month - early_dt.month
+def getAge(param_file_path, repo_path):
+   totalCountForChurn   = 0
+   monthDict            = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06',
+                           'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
+
+   cdCommand            = "cd " + repo_path + " ; "
+   theFile              = os.path.relpath(param_file_path, repo_path)
+   commitCommand        = "git log  --format=%cd " + theFile + " | awk '{ print $2 $3 $5}' | sed -e 's/ /,/g'"
+   command2Run          = cdCommand + commitCommand
+
+   dt_churn_output = subprocess.check_output(['bash','-c', command2Run])
+   dt_churn_output = dt_churn_output.split('\n')
+   dt_churn_output = [x_ for x_ in dt_churn_output if x_!='']
+   #print dt_churn_output
+   monthList = [dob[0:3] for dob in  dt_churn_output]
+   yearist = [dob[-4:] for dob in  dt_churn_output]
+   monthAndYeatList = [dob[-4:] + '-' + monthDict[dob[0:3]] for dob in dt_churn_output]
+   monthAndYeatList.sort()
+   #print monthAndYeatList
+   earliesttMonth  = monthAndYeatList[0]
+   latesttMonth    = monthAndYeatList[-1]
+   age = str(calculateMonthDiffFromTwoDates(earliesttMonth, latesttMonth))
+   #print age
+   return age
+
 
 def getProcessMetrics(file_path_p, repo_path_p):
     #get commit count
     COMM = getCommitCount(file_path_p, repo_path_p)
+    #get age
+    AGE  = getAge(file_path_p, repo_path_p)
--- hg_process_extractor.py
+++ hg_process_extractor.py
@@ -27,6 +27,45 @@ def getCommitCount(param_file_path, repo_path):
 
 
 
+def calculateMonthDiffFromTwoDates(early, latest):
+    from datetime import datetime
+    early_year   = early.split('-')[0]
+    latest_year  = latest.split('-')[0]
+    early_month  = early.split('-')[1]
+    latest_month = latest.split('-')[1]
+    early_day    = early.split('-')[-1]
+    latest_day   = latest.split('-')[-1]
+
+    early_dt     = datetime(int(early_year), int(early_month), int(early_day))
+    latest_dt    = datetime(int(latest_year), int(latest_month), int(latest_day))
+
+    return (latest_dt.year - early_dt.year)*12 + latest_dt.month - early_dt.month
+def getAge(param_file_path, repo_path):
+   totalCountForChurn   = 0
+   monthDict            = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06',
+                           'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
+
+   cdCommand            = "cd " + repo_path + " ; "
+   theFile              = os.path.relpath(param_file_path, repo_path)
+   commitCommand        = " hg churn --dateformat '%Y-%m-%d' " +  theFile +    " | awk '{print $1 }' "
+   command2Run          = cdCommand + commitCommand
+
+   dt_churn_output = subprocess.check_output(['bash','-c', command2Run])
+   dt_churn_output = dt_churn_output.split('\n')
+   monthAndYeatList = [x_ for x_ in dt_churn_output if x_!='']
+   #print monthAndYeatList
+   monthAndYeatList.sort()
+   print monthAndYeatList
+   earliesttMonth  = monthAndYeatList[0]
+   latesttMonth    = monthAndYeatList[-1]
+   age = str(calculateMonthDiffFromTwoDates(earliesttMonth, latesttMonth))
+   print age
+   return age
+
+
+
 def getProcessMetrics(file_path_p, repo_path_p):
     #get commit count
     COMM = getCommitCount(file_path_p, repo_path_p)
+    #get AGE
+    AGE = getAge(file_path_p, repo_path_p)
