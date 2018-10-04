4e0cdc5369c4af296cbb281bcab256cf09767c43
Akond Rahman <akond.rahman.buet@gmail.com>
2018-10-04 01:16:49 -0400

    initial push

--- fse2019/main_extractor.py
+++ fse2019/main_extractor.py
@@ -26,20 +26,19 @@ def getAllProcessMetricsForSingleFile(full_path_param, repo_path_param, org_of_f
 
 
 
-def getAllProcessMetricForAllFiles(pupp_map_dict_param, datasetFile2Save, org_name):
+def getAllProcessMetricForAllFiles(pupp_map_dict_param, datasetFile2Save, org_name, meenely_dict):
    str2ret=''
    fileCount = 0
-   '''
-   LOAD the file to programmer dicts first
-   '''
-   #moz_prog_to_file_dist = process_metric_utility.getMercurialProgToFileMapping(repo_)
+
    for file_, details_ in pupp_map_dict_param.items():
-     if (file_!= 'WTF'):
+     if (file_!= 'WTF') and (file_ in meenely_dict):
         fileCount = fileCount + 1
         repo_                    = details_[1]
         defect_status            = details_[0]
         print "Analyzing ... \nfile#{}\ndefect status:{}\nfile:{}\nrepo:{}".format(fileCount, defect_status, file_, repo_)
         all_metric_for_this_file = getAllProcessMetricsForSingleFile(file_, repo_, org_name)
+        dev_met_, col_met_ = meenely_dict[file_]
+        all_metric_for_this_file = all_metric_for_this_file + str(dev_met_) + ',' + str(col_met_) + ','
         str2ret = str2ret + all_metric_for_this_file + defect_status + '\n'
         print "="*75
    dump_stats = process_metric_utility.createDataset(str2ret, datasetFile2Save)
@@ -51,29 +50,39 @@ for dataset generation
 '''
 
 ### INPUT
-
+theCompleteCategFile='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Mirantis_Categ_For_DB.csv'
 init_dataset_file='/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/ICSE19_TSE/MIR_FUL_PRO.csv'
 ORG = 'MIRANTIS'
 
+
+# theCompleteCategFile='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Mozilla.Final.Categ.csv'
 # init_dataset_file='/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/ICSE19_TSE/MOZ_FUL_PRO.csv'
+# ORG = 'MOZILLA'
+
+# theCompleteCategFile='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Openstack.WithoutBadBoys.Final.Categ.csv'
 # init_dataset_file='/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/ICSE19_TSE/OST_FUL_PRO.csv'
+# ORG = 'OPENSTACK'
+
+# theCompleteCategFile='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Wikimedia.Final.Categ.csv'
 # init_dataset_file='/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/ICSE19_TSE/WIK_FUL_PRO.csv'
+# ORG = 'WIKIMEDIA'
 
 
 ### OUTPUT
 
-datasetFile2Save='/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/MIR_CHANGE_DATASET.csv'
+datasetFile2Save='/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/MIR_FULL_DATASET.csv'
 
 
 
 
 print "Started at:", process_metric_utility.giveTimeStamp()
-fullPuppMap   = process_metric_utility.getPuppetFileDetails(init_dataset_file)
+fullPuppMap   = process_metric_utility.getPuppetFileDetails(theCompleteCategFile)
+meenely_dict  = process_metric_utility.getMeenelyDetails(init_dataset_file)
 print "Loaded the defect mapping of files ... "
 print "-"*100
 
 
-str_ = getAllProcessMetricForAllFiles(fullPuppMap, datasetFile2Save, ORG)
+str_ = getAllProcessMetricForAllFiles(fullPuppMap, datasetFile2Save, ORG, meenely_dict)
 print "-"*100
 print "We analyzed {} Puppet files".format(len(fullPuppMap))
 print "-"*100
--- fse2019/process_metric_utility.py
+++ fse2019/process_metric_utility.py
@@ -46,17 +46,14 @@ def giveTimeStamp():
   strToret = datetime.datetime.fromtimestamp(tsObj).strftime('%Y-%m-%d %H:%M:%S')
   return strToret
 
+
 def createDataset(str2Dump, datasetNameParam):
    headerOfFile0='org,file_,'
-   #headerOfFile1='COMM,AGE,DEV,AVGTIMEOFEDITS,ADDPERLOC,'
-   headerOfFile1='COMM,AGE,DEV,ADDPERLOC,'
-   #headerOfFile2='DELPERLOC,ADDNORM,DELNORM,AVGCHNG,MINOR,OWN,SCTR,'
-   headerOfFile2='DELPERLOC,SUMCHNG,TOTCHNGPERLOC,AVGCHNG,MINOR,SCTR,'
-   #headerOfFile3='COMM_SIZE,MT_PP, MT_NON_PP,'
-   headerOfFile3='MT_PP,MT_NON_PP,'
-   headerOfFile4='defect_status'
+   headerOfFile1='ADD_PER_COM,DEL_PER_COM,TOT_PER_COM,DEVS,'
+   headerOfFile2='MINORS,OWNER_LINES,SCATERNESS,DEV_MET,COLA_MET,'
+   headerOfFile3='defect_status'
 
-   headerStr = headerOfFile0 + headerOfFile1 + headerOfFile2  +  headerOfFile3 + headerOfFile4
+   headerStr = headerOfFile0 + headerOfFile1 + headerOfFile2  +  headerOfFile3 
 
    str2Write = headerStr + '\n' + str2Dump
    return dumpContentIntoFile(str2Write, datasetNameParam)
@@ -66,71 +63,6 @@ def getDatasetFromCSV(fileParam, dataTypeFlag=True):
   else:
         data_set_to_return = np.genfromtxt(fileParam, delimiter=',', skip_header=1,  dtype='str')
   return data_set_to_return
-
-'''
-for opnestack bad boys
-'''
-def getScatterFromBadBoyFile(fileName, sloc):
-   blame_output =[]
-   '''
-   output list
-   '''
-   lineNoProb        = []
-   lineNoCnt         = []
-   '''
-   '''
-   lineNoProb = []
-   with open(theCompleteCategFile, 'rU') as file_:
-      reader_ = csv.reader(file_)
-      for row_ in reader_:
-          blame_output.append(row_[0])
-   line_chng_dict    = dict(Counter(blame_output))
-   #print line_chng_dict
-   for lineNo in xrange(sloc):
-       line_key  = str(lineNo + 1)
-       if (line_key in line_chng_dict):
-          line_cnt  = line_chng_dict[line_key]
-       else:
-          line_cnt  = 0
-       line_prob = float(line_cnt)/float(sloc)
-       lineNoProb.append(line_prob) ### Version 1
-       lineNoCnt.append(line_cnt)   ### Version 2
-   #print "len:{}, list:{}, loc:{}".format(len(lineNoProb), lineNoProb, sloc)
-   scatterness_prob = round(entropy(lineNoProb), 5)  ##Version 1
-   scatterness_cnt  = round(entropy(lineNoCnt), 5)  ##Version 2
-   '''
-   handling -inf, inf
-   '''
-   if((scatterness_cnt == float("-inf")) or (scatterness_cnt == float("inf"))):
-     scatterness_cnt = float(0)
-   return scatterness_cnt
-'''
-list missing files
-'''
-def listMissingFiles(dataset_1, dataset_2):
-    static_datset =[]
-    process_dataset = []
-    str_ = ''
-    with open(dataset_1, 'rU') as file_:
-          reader_ = csv.reader(file_)
-          next(reader_, None)
-          for row_ in reader_:
-              static_datset.append(row_[1])
-    print len(static_datset)
-    print(len(np.unique(static_datset)))
-    print(Counter(static_datset))
-    with open(dataset_2, 'rU') as file_2:
-          reader2_ = csv.reader(file_2)
-          next(reader2_, None)
-          for row_2 in reader2_:
-              process_dataset.append(row_2[1])
-    missing_files = [x for x in static_datset if x not in process_dataset]
-    for file_name in missing_files:
-        str_ = str_ + file_name + ',' + '\n'
-    dumpContentIntoFile(str_, '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/DoAgainOpenstack.csv')
-
-
-
 
 def createOutputDirectory(dirParam):
   if not os.path.exists(dirParam):
@@ -226,204 +158,23 @@ def createLogTransformedFeatures(allFeatureParam):
   print "-"*50
   for ind_ in xrange(dataset_rows):
     features_for_this_index = allFeatureParam[ind_, :]
-    ## do the log tranform on the extracted index
-    ## the following code handles the issue for zero values
     log_transformed_features_for_index = [math.log1p(x_) for x_ in features_for_this_index]
     log_transformed_feature_dataset_to_ret.append(log_transformed_features_for_index)
-  ## convert to numpy  array
   log_transformed_feature_dataset_to_ret = np.array(log_transformed_feature_dataset_to_ret)
   return log_transformed_feature_dataset_to_ret
-'''
-YEAR WISE DATA EXTRACTION
-'''
-def getYear(single_date):
-    dt2ret = single_date.split('-')[0]
-    return dt2ret
 
-def getTimeInfo(id_param, repo_param):
-    dict2see = {}
-    if repo_param.endswith('/'):
-       repo_param = repo_param
-    else:
-       repo_param = repo_param + '/'
-    file2read = repo_param + 'fullThrottle_msg_file_map.csv'
-    with open(file2read, 'rU') as f:
-         reader_ = csv.reader(f)
-         for row in reader_:
-             id_       = row[0]
-             ts_    = row[2]
-             dict2see[id_] = ts_
-    return dict2see[id_param]
-
-
-
-def getYearFileCount(df_param):
-    _df = pd.DataFrame(df_param, columns=['ID', 'REPO', 'FILE', 'TIME', 'DEFECTSTATUS'])
-    _df['YEAR']  = _df['TIME'].apply(getYear)
-    _df         = _df.sort(['YEAR'])
-    _df_year_cnt = _df.groupby(['YEAR'])[['FILE']].count()
-
-    y_list         = _df_year_cnt.index.get_level_values('YEAR').tolist()
-    y_c_list       = _df_year_cnt['FILE'].tolist()
-    return y_list, y_c_list
-
-
-
-def getAllYearsFromDataset(categ_file_param):
-       str2write = ''
-       df_list = []
-       '''
-       dicionary to hold files for each year
-       '''
-       year_file_dict, defect_file_dict, repo_file_dict = {}, {}, {}
-       with open(categ_file_param, 'rU') as f:
-         reader_ = csv.reader(f)
-         next(reader_, None)
-         for row in reader_:
-             id_       = row[0]
-             repo_     = row[1]
-             categ_    = row[3]
-             if categ_=='N':
-                 defect_status = '0'
-             else:
-                 defect_status = '1'
-             filepath_ = row[4]
-             time_ = getTimeInfo(id_, repo_)
-             time2write = time_.split(' ')[0]
-             df_list.append((id_, repo_, filepath_, time2write, defect_status))
-             '''
-             dictionary year
-             '''
-             y2write = getYear(time2write)
-             if y2write not in year_file_dict:
-                 year_file_dict[y2write] = [filepath_]
-             else:
-                 year_file_dict[y2write] = year_file_dict[y2write] + [filepath_]
-             '''
-             dictionary files
-             '''
-             if filepath_ not in defect_file_dict:
-                 defect_file_dict[filepath_] = defect_status
-             '''
-             dictionary repos
-             '''
-             if filepath_ not in repo_file_dict:
-                 repo_file_dict[filepath_] = repo_
-       # get all years, and files in the year for the dataset
-       y_list, y_c_list = getYearFileCount(df_list)
-       return y_list, year_file_dict, defect_file_dict, repo_file_dict
-
-'''
-TIME DATASET SPLITTING EXTRACT ZONE
-'''
-
-def getAllRepos(file_name):
-       all_repos = []
-       with open(file_name, 'rU') as f:
-         reader_ = csv.reader(f)
-         for row in reader_:
-             all_repos.append(row[0])
-       return all_repos
-
-'''
-TIME METRIC EXTRACT ZONE
-'''
-def getRepoToFileMapping(categ_output_file):
-    repo_file_dict={}
-    with open(categ_output_file, 'rU') as f:
-      reader_ = csv.reader(f)
+def getMeenelyDetails(meenely_file):
+    dict_ = {}
+    with open(meenely_file, 'rU') as file_:
+      reader_ = csv.reader(file_)
       next(reader_, None)
-      for row in reader_:
-          repo_     = row[1]
-          filepath_ = row[4]
-          if repo_ not in repo_file_dict:
-             repo_file_dict[repo_] = filepath_
-          else:
-             repo_file_dict[repo_] = repo_file_dict[repo_] + filepath_
-    return repo_file_dict
+      for row_ in reader_:
+        dev_met_of_file = row_[10]
+        col_met_of_file = row_[11]
+        
+        path_of_file    = row_[1]
 
-
-def getRecursivelyPPFilles(repo_path):
-        all_files = []
-        for root, subFolders, _files in os.walk(repo_path):
-            for file_ in _files:
-                if ('.pp' in file_):
-                   the_pp_file = os.path.join(root, file_)
-                   if('EXTRA_AST' not in the_pp_file):
-                     all_files.append(the_pp_file)
-        return all_files
-
-
-def getDefectData(year_param, categ_file_param):
-       defect_file_dict, output_dict = {}, {}
-       with open(categ_file_param, 'rU') as f:
-         reader_ = csv.reader(f)
-         next(reader_, None)
-         for row in reader_:
-             id_       = row[0]
-             file_     = row[4]
-             repo_     = row[1]
-             categ_    = row[3]
-             time_ = getTimeInfo(id_, repo_)
-             y2check = time_.split(' ')[0]
-             y2check = y2check.split('-')[0]
-             #print y2check, year_param
-             if (y2check==year_param):
-                 if file_ not in defect_file_dict:
-                    defect_file_dict[file_] = [categ_]
-                 else:
-                    defect_file_dict[file_] =  defect_file_dict[file_] + [categ_]
-       for file_, categs in defect_file_dict.iteritems():
-           categs_ = np.unique(categs)
-           if((categs_[0]=='N') and (len(categs_)==1)):
-               defect_satus = '0'
-           else:
-               defect_satus = '1'
-           output_dict[file_] = defect_satus
-       return output_dict
-def getAllYearsFromCategFile(categ_file_param):
-       df_list = []
-       with open(categ_file_param, 'rU') as f:
-         reader_ = csv.reader(f)
-         next(reader_, None)
-         for row in reader_:
-             id_       = row[0]
-             repo_     = row[1]
-             filepath_ = row[4]
-             categ_    = row[3]
-             if categ_=='N':
-                 defect_status = '1'
-             else:
-                 defect_status = '0'
-             time_ = getTimeInfo(id_, repo_)
-             time2write = time_.split(' ')[0]
-
-             df_list.append((id_, repo_, filepath_, time2write, defect_status))
-
-       # get all years, and files in the year for the dataset
-       y_list, y_c_list = getYearFileCount(df_list)
-       return y_list
-'''
-PREDICTION TIME SPLITTING
-'''
-
-def getFeaturesAndLabels(file_name_param):
-       training_dataset = getDatasetFromCSV(file_name_param)
-       full_rows, full_cols = np.shape(training_dataset)
-       feature_cols = full_cols - 1  ## the last column is defect status, so one column to skip
-       training_features = training_dataset[:, 2:feature_cols]
-       '''
-       lets transform all the features via log transformation
-       '''
-       log_transformed_train_features = createLogTransformedFeatures(training_features)
-       '''
-       get labels
-       '''
-       dataset_for_labels = getDatasetFromCSV(file_name_param)
-       label_cols = full_cols - 1
-       all_labels  =  dataset_for_labels[:, label_cols]
-       defected_file_count     = len([x_ for x_ in all_labels if x_==1.0])
-       non_defected_file_count = len([x_ for x_ in all_labels if x_==0.0])
-       print "No of. defects={}, non-defects={}".format(defected_file_count, non_defected_file_count)
-       print "-"*50
-       return log_transformed_train_features, all_labels
+        if path_of_file not in dict_:
+            dict_[path_of_file] = (dev_met_of_file, col_met_of_file)
+    
+    return dict_
