95b2d4839c3bac97e5ed620998b0d7fc462a7da9
Akond Rahman <akond.rahman.buet@gmail.com>
2019-02-10 11:22:46 -0500

    more fse work for paper

--- fse2019/extra_paper_work.py
+++ fse2019/extra_paper_work.py
@@ -22,28 +22,68 @@ def showMetricDist(the_fil, metric_name, ds_name):
     defect_df = the_df_[the_df_['defect_status']==1]
 
     metric_vals = defect_df[metric_name].tolist()
+    if metric_name == 'OWNER_LINES':
+       metric_vals = [round(x_, 1) for x_ in metric_vals]
+
     tot_cnt = len(metric_vals)
     
     metric_dist = dict(Counter(metric_vals)) 
     for k_, v_ in metric_dist.iteritems():
         perc_valu = (float(v_)/float(tot_cnt))*100
         file_name =  ds_name.split('/')[-1] 
-        print '[DATASET:::{}:::]{} developers for {}% defective scripts'.format(file_name, k_, perc_valu )
+        print '[DATASET:::{}:::]{}  for {}% defective scripts'.format(file_name, k_, perc_valu )
         strToWrite = strToWrite + str(k_) + ',' + str(perc_valu) + ',' + '\n'
     
     dumpContentIntoFile(strToWrite, '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/' + file_name + '_' + metric_name + '.csv')
 
+def dumpCount(the_fil, metric_name, output_file,  ds_name):
+    strToWrite = ''
+    
+
+    the_df_ = pd.read_csv(the_fil)
+    defect_df = the_df_[the_df_['defect_status']==1]
+
+    metric_vals = defect_df[metric_name].tolist()
+    if metric_name == 'OWNER_LINES':
+       metric_vals = [round(x_, 1) for x_ in metric_vals]
+
+    tot_cnt = len(metric_vals)
+    
+    for x_ in metric_vals: 
+        strToWrite = strToWrite + ds_name + ',' + str(x_) + ',' + '\n'
+
+    file_name = output_file.split('/')[-1] 
+    dumpContentIntoFile(strToWrite, '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/' + file_name + '_' + metric_name + '.csv')
 
 if __name__=='__main__':
+    '''
+    get percentages 
+    '''
+
     # the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_MIR_FULL_DATASET.csv'
-    # showMetricDist(the_fil, 'DEVS', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/MIRA_DEVS_DIST.csv')
+    # showMetricDist(the_fil, 'COLA_MET', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/MIRA_COLA_MET_DIST.csv')
     
     # the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_MOZ_FULL_DATASET.csv'
-    # showMetricDist(the_fil, 'DEVS', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/MOZI_DEVS_DIST.csv')
+    # showMetricDist(the_fil, 'COLA_MET', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/MOZI_COLA_MET_DIST.csv')
 
     # the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_OST_FULL_DATASET.csv'
-    # showMetricDist(the_fil, 'DEVS', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/OSTK_DEVS_DIST.csv')    
+    # showMetricDist(the_fil, 'COLA_MET', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/OSTK_COLA_MET_DIST.csv')    
 
-    the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_WIK_FULL_DATASET.csv'
-    showMetricDist(the_fil, 'DEVS', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/WIKI_DEVS_DIST.csv')        
+    # the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_WIK_FULL_DATASET.csv'
+    # showMetricDist(the_fil, 'COLA_MET', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/WIKI_COLA_MET_DIST.csv')        
 
+    '''
+    dump raw coutnt 
+    '''
+
+    # the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_MIR_FULL_DATASET.csv'
+    # dumpCount(the_fil, 'DEVS', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/MIRA_DEVS_RAW_CNT.csv', 'MIR')    
+
+    # the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_MOZ_FULL_DATASET.csv'
+    # dumpCount(the_fil, 'DEVS', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/MOZ_DEVS_RAW_CNT.csv', 'MOZ')    
+
+    # the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_OST_FULL_DATASET.csv'
+    # dumpCount(the_fil, 'DEVS', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/OSTK_DEVS_RAW_CNT.csv', 'OST')    
+
+    # the_fil = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/FIGSHARE/REDUCED_WIK_FULL_DATASET.csv'
+    # dumpCount(the_fil, 'DEVS', '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/output/FSE2019/WIK_DEVS_RAW_CNT.csv', 'WIK')    
