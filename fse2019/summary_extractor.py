2d2ed599f66190d9a8b22d1418012ab7560d5a5c
Akond Rahman <akond.rahman.buet@gmail.com>
2020-02-21 10:01:23 -0600

    python 3 stuff

--- fse2019/summary_extractor.py
+++ fse2019/summary_extractor.py
@@ -1,7 +1,7 @@
 '''
 get summary stats from datasets
 Akond Rahman
-Nov 05, 2018
+Feb 21, 2020 
 '''
 from scipy import stats
 import pandas as pd
@@ -9,10 +9,10 @@ import numpy as np
 import cliffsDelta
 
 
-mirantis_file  ="/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/REDUCED_MIR_FULL_DATASET.csv"
-mozilla_file   ="/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/REDUCED_MOZ_FULL_DATASET.csv"
-openstack_file ="/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/REDUCED_OST_FULL_DATASET.csv"
-wikimedia_file ="/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/FSE2019/REDUCED_WIK_FULL_DATASET.csv"
+mirantis_file  ="/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/IaC/iac-ncsu/ProcessInIaC/dataset/FSE2019/FIGSHARE/_MIR_FULL_DATASET.csv"
+mozilla_file   ="/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/IaC/iac-ncsu/ProcessInIaC/dataset/FSE2019/FIGSHARE/_MOZ_FULL_DATASET.csv"
+openstack_file ="/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/IaC/iac-ncsu/ProcessInIaC/dataset/FSE2019/FIGSHARE/_OST_FULL_DATASET.csv"
+wikimedia_file ="/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/IaC/iac-ncsu/ProcessInIaC/dataset/FSE2019/FIGSHARE/_WIK_FULL_DATASET.csv"
 
 
 
@@ -25,10 +25,10 @@ def giveTimeStamp():
 
 dataset_files = [mirantis_file, mozilla_file, openstack_file, wikimedia_file]
 
-print "Started at:", giveTimeStamp()
+print("Started at:", giveTimeStamp() )
 for dataset_file in dataset_files:
     name = dataset_file.split('/')[-1]
-    print "Dataset:", name
+    print("Dataset:", name )
     df2read = pd.read_csv(dataset_file)
     #print df2read.head()
 
@@ -42,27 +42,25 @@ for dataset_file in dataset_files:
            '''
            data_for_feature = df2read[feature_]
            median_, mean_, total_ = np.median(data_for_feature), np.mean(data_for_feature), sum(data_for_feature)
-           print "Feature:{}, [ALL DATA] median:{}, mean:{}, sum:{}".format(feature_, median_, mean_, total_  )
-           print '='*50
+           print("Feature:{}, [ALL DATA] median:{}, mean:{}, sum:{}".format(feature_, median_, mean_, total_  ) )
+           print('='*50)
            defective_vals_for_feature     = df2read[df2read['defect_status']==1][feature_]
            non_defective_vals_for_feature = df2read[df2read['defect_status']==0][feature_]
            '''
            summary time
            '''
-           print 'THE FEATURE IS:', feature_
-           print '='*25
-           #print "Defective values stats: \n", defective_vals_for_feature.describe()
-           print "Defective values [MEDIAN]:{}, [MEAN]:{}".format(np.median(list(defective_vals_for_feature)), np.mean(list(defective_vals_for_feature)))
-           #print "Non defective values stats: \n", non_defective_vals_for_feature.describe()
-           print "Non Defective values [MEDIAN]:{}, [MEAN]:{}".format(np.median(list(non_defective_vals_for_feature)), np.mean(list(non_defective_vals_for_feature)))
-           
+           print('THE FEATURE IS:', feature_ )
+           print('='*25)
+           print("Defective values [MEDIAN]:{}, [MEAN]:{}, [MAX]:{}, [MIN]:{}".format(np.median(list(defective_vals_for_feature)), np.mean(list(defective_vals_for_feature)), max(list(defective_vals_for_feature) ), min(list(defective_vals_for_feature) )   ) )
+           print("Non Defective values [MEDIAN]:{}, [MEAN]:{}, [MAX]:{}, [MIN]:{}".format(np.median(list(non_defective_vals_for_feature)), np.mean(list(non_defective_vals_for_feature)),  max(list(non_defective_vals_for_feature)),  min(list(non_defective_vals_for_feature)) ) )
+          
            if feature_=='OWNER_LINES':
               TS, p = stats.mannwhitneyu(list(non_defective_vals_for_feature), list(defective_vals_for_feature), alternative='greater')
            else:
               TS, p = stats.mannwhitneyu(list(defective_vals_for_feature), list(non_defective_vals_for_feature), alternative='greater')
 
            cliffs_delta = cliffsDelta.cliffsDelta(list(defective_vals_for_feature), list(non_defective_vals_for_feature))
-           print 'Feature:{}, pee value:{}, cliffs:{}'.format(feature_, p, cliffs_delta)
-           print '='*50
-    print '*'*100
-print "Ended at:", giveTimeStamp()
+           print('Feature:{}, pee value:{}, cliffs:{}'.format(feature_, p, cliffs_delta) )
+           print('='*50)
+    print('*'*100)
+print("Ended at:", giveTimeStamp())
